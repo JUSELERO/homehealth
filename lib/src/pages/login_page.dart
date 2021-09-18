5480ec463e883ca37db057bbe509802666b0ceaf
Laura <laura.svilla21@gmail.com>
2021-09-17 23:02:26 -0500

    Cajas de texto en Login

--- lib/src/pages/login_page.dart
+++ lib/src/pages/login_page.dart
@@ -1,14 +1,39 @@
 import 'package:flutter/material.dart';
 
-import 'home_page.dart';
+//import 'home_page.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String _nombre = "";
+  String _email = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: <Widget>[
           _crearFondo(context),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 20.0, vertical: 50.0),
+            child: ListView(
+              // Crea los cuadros de texto
+              padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+              children: <Widget>[
+                _crearInput(),
+                Divider(),
+                _crearEmail(),
+                Divider(),
+                _passwordField(),
+                Divider(),
+                _registrarPersona(),
+              ],
+            ),
+          ),
         ],
       ),
       floatingActionButton: FloatingActionButton.extended(
@@ -18,6 +43,87 @@ class LoginPage extends StatelessWidget {
         label: const Text('Regresar'),
       ),
     );
+  }
+
+  Widget _crearInput() {
+    TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
+    return TextField(
+      autofocus: true,
+      style: style,
+      decoration: InputDecoration(
+          fillColor: Colors.white,
+          filled: true, //Colorea fondo de blanco
+          counter: Text('Letras ${_nombre.length}'),
+          hintText: 'Ingresa tu nombre',
+          labelText: 'Nombre',
+          helperText: 'Por favor, ingresa tu nombre',
+          suffixIcon: Icon(Icons.accessibility),
+          icon: Icon(Icons.account_circle),
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+      onChanged: (valor) {
+        setState(() {
+          _nombre = valor;
+        });
+      },
+    );
+  }
+
+  Widget _crearEmail() {
+    TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      style: style,
+      decoration: InputDecoration(
+          fillColor: Colors.white,
+          filled: true,
+          hintText: 'Ingresa tu correo electrónico',
+          labelText: 'Email',
+          helperText: 'Por favor, ingresa tu email',
+          suffixIcon: Icon(Icons.alternate_email),
+          icon: Icon(Icons.email),
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+      onChanged: (valor) {
+        setState(() {
+          _email = valor;
+        });
+      },
+    );
+  }
+
+  Widget _passwordField() {
+    TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
+    return TextField(
+      obscureText: true,
+      keyboardType: TextInputType.emailAddress,
+      style: style,
+      decoration: InputDecoration(
+          fillColor: Colors.white,
+          filled: true,
+          hintText: 'Ingresa tu contraseña',
+          labelText: 'Password',
+          helperText: 'Por favor, ingresa tu contraseña',
+          suffixIcon: Icon(Icons.lock_open),
+          icon: Icon(Icons.lock),
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+      onChanged: (valor) {
+        setState(() {
+          _email = valor;
+        });
+      },
+    );
+  }
+
+//
+  Widget _registrarPersona() {
+    return ListTile(
+        title: Text('El nombre es: $_nombre'),
+        subtitle: Text('Email: $_email'));
   }
 }
 
@@ -60,19 +166,19 @@ Widget _crearFondo(BuildContext context) {
         left: -100.0,
         child: circulo,
       ),
-      Container(
-        padding: EdgeInsets.only(top: 50.0),
-        child: Column(
-          children: <Widget>[
-            Icon(Icons.person_pin_circle, color: Colors.white, size: 100.0),
-            SizedBox(
-              height: 10.0,
-              width: double.infinity,
-            ),
-            Text('sebastian ', style: TextStyle(color: Colors.white)),
-          ],
-        ),
-      )
+      // Container(
+      //   padding: EdgeInsets.only(top: 50.0),
+      //   child: Column(
+      //     children: <Widget>[
+      //       Icon(Icons.person_pin_circle, color: Colors.white, size: 100.0),
+      //       SizedBox(
+      //         height: 10.0,
+      //         width: double.infinity,
+      //       ),
+      //       Text('sebastian ', style: TextStyle(color: Colors.white)),
+      //     ],
+      //   ),
+      // )
     ],
   );
 }
