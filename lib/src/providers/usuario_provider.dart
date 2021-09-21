e1c05325ac343296c099d32cd51fbdf3f9a50d36
JUSELERO <juansebastianleon5@gmail.com>
2021-09-21 07:04:38 -0500

    se configura el registro y login la base de datos HomeHelp

--- lib/src/providers/usuario_provider.dart
+++ lib/src/providers/usuario_provider.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 
 class UsuarioProvider {
   //cambiar dependiendo de  la base de datosd
-  final String _firebaseToken = 'AIzaSyBZRCn6fb0BXeYMcWdDrdoB2e36vg31ybc';
+  final String _firebaseToken = 'AIzaSyDgyaFe5aZooh0srj9mJ9YHKoDE1YgeHlM';
 
   Future<Map<String, dynamic>> login(String email, String password) async {
     final authData = {
